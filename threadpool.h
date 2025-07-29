c8e6cb8318b3a17382078f38e3a85d86bd526b14
VatsalIITBHU <vatsal.patni.cd.civ23@itbhu.ac.in>
2025-07-29 21:48:58 +0530

    Delete threadpool.h

--- threadpool.h
+++
@@ -1,8 +0,0 @@
-// Minimal ThreadPool placeholder - not used in current code
-#ifndef THREADPOOL_H
-#define THREADPOOL_H
-
-// You could implement a thread pool here if you want smarter worker management.
-// Currently, raw threads are directly created in main().
-
-#endif // THREADPOOL_H
